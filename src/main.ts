b627ab0b1606d68787853163281437517cf109a8
Minhazur Rahman Rafi <rafi.rahman03@yahoo.com>
2026-07-16 21:27:17 +0600

    fix: added local host

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,11 @@ import { ResponseInterceptor, AllExceptionsFilter } from './common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://salmon-forest-0428d1000.7.azurestaticapps.net',
+    origin: [
+      'https://salmon-forest-0428d1000.7.azurestaticapps.net',
+      'http://localhost:4173/',
+      'http://localhost:5173/',
+    ],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
